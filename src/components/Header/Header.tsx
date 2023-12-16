459377d1bb3044378b1e4c3cc3a3bb2199355297
Denys Borysiuk <denisborisiuk@gmail.com>
2023-12-16 22:09:14 +0200

    add favicon

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,11 @@
+import Logo from '@/components/ui/Logo';
+
 const Header = () => {
-  return <header></header>;
+  return (
+    <header className=" bg-slate-600">
+      <Logo />
+    </header>
+  );
 };
 
 export default Header;
--- src/components/ui/Logo/Logo.tsx
+++ src/components/ui/Logo/Logo.tsx
@@ -1,5 +1,12 @@
+import Link from 'next/link';
+import Image from 'next/image';
+
 const Logo = () => {
-  return <div></div>;
+  return (
+    <Link href="/">
+      <Image src="/icons/logo.svg" width={61} height={36} alt="logo" priority />
+    </Link>
+  );
 };
 
 export default Logo;
