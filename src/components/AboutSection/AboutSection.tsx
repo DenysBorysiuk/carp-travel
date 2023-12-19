ed7260bbe3df67643d14ce0163cd3c7ea63be78e
Denys Borysiuk <denisborisiuk@gmail.com>
2023-12-19 20:12:04 +0200

    about sm and md done

--- src/components/AboutSection/AboutSection.tsx
+++ src/components/AboutSection/AboutSection.tsx
@@ -5,15 +5,20 @@ import AboutText from '@/components/ui/AboutText';
 const AboutSection = () => {
   return (
     <section className="about-bg" id="about">
-      <div className="container">
-        <SectionTitle className={'mb-[8px]'} data={data.title} />
-        <AboutText className={'mb-[24px] w-[180px]'} data={data.descriptions[0]} />
-        <AboutText className={'mb-[40px] w-[180px]'} data={data.descriptions[1]} />
-        <h3 className="mb-[40px] block w-[180px] font-extralight">
-          <span className="block font-normal  uppercase">{data.subtitle.textAccent}</span>
-          {data.subtitle.text}
+      <div className="container relative">
+        <div className="mb-[40px] md:mb-[64px] md:flex md:h-[445px] md:flex-col md:flex-wrap">
+          <SectionTitle className={'mb-[8px]'} data={data.title} />
+          <div className="w-[180px] md:w-[220px] md:pt-[9px]">
+            <AboutText className={'mb-[24px] md:mb-[16px]'} data={data.descriptions[0]} />
+            <AboutText className={''} data={data.descriptions[1]} />
+          </div>
+        </div>
+        <h3 className="mb-[40px] ml-auto flex w-[180px] flex-col md:absolute md:top-[385px]">
+          <span className="uppercase">{data.subtitle.textAccent[0]}</span>
+          <span className="ml-auto uppercase">{data.subtitle.textAccent[1]}</span>
+          <span className="font-extralight tracking-[-0.14px]">{data.subtitle.text}</span>
         </h3>
-        <AboutText className={''} data={data.descriptions[2]} />
+        <AboutText className={'md:ml-auto md:w-[463px]'} data={data.descriptions[2]} />
       </div>
     </section>
   );
