32852d0d8531f92c8f378611fe260c44711c8665
Denys Borysiuk <denisborisiuk@gmail.com>
2024-01-03 16:58:54 +0200

    fix margins xl

--- src/components/ContactDetails/ContactDetails.tsx
+++ src/components/ContactDetails/ContactDetails.tsx
@@ -15,7 +15,9 @@ const ContactDetails = () => {
             </li>
           ))}
         </ul>
-        <p className="mt-[4px] text-[12px] xl:leading-loose">{phones.text}</p>
+        <p className="mt-[4px] text-[12px] leading-[1.66] xl:mt-[3px] xl:leading-loose">
+          {phones.text}
+        </p>
       </div>
       <address className="flex items-center gap-[20px] not-italic smOnly:ml-[16px]">
         <a className="contacts-link" href={email.href}>
--- src/components/ui/Socials/Socials.tsx
+++ src/components/ui/Socials/Socials.tsx
@@ -6,7 +6,7 @@ const { text, links } = socials;
 const Socials = () => {
   return (
     <div className="flex gap-[20px] xl:mr-[30px] xl:flex-row-reverse smOnly:mr-[15px] smOnly:justify-end">
-      <p className="mt-[4px] text-[12px] xl:leading-loose">{text}</p>
+      <p className="mt-[2px] text-[12px] leading-[1.66] md:mt-[4px] xl:leading-loose">{text}</p>
       <ul className=" xl:text-right">
         {links.map(({ label, href }) => (
           <li key={label}>
