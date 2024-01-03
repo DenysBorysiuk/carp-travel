6bcef9d2cc74080da12c0a9c1d86b8925b97c46d
Denys Borysiuk <denisborisiuk@gmail.com>
2024-01-03 17:44:14 +0200

    fix btn width

--- src/components/ServicesList/ServicesList.tsx
+++ src/components/ServicesList/ServicesList.tsx
@@ -6,16 +6,17 @@ const ServicesList = ({ list, currentSlide, onSlideTo, slogan }: ServicesListPro
   return (
     <ul className="flex flex-col gap-[16px] xl:gap-[24px]">
       {list.map((item, index) => (
-        <li className="relative" key={item}>
+        <li className="relative w-fit" key={item}>
           <button
-            className={`relative flex w-[169px] items-center
-                     text-left text-[20px] font-extralight uppercase leading-[0.85] transition-all 
-                     duration-300 md:w-[186px] md:text-[22px] md:leading-[0.82]
-                    ${
-                      currentSlide === index
-                        ? 'btn ml-[16px] font-medium'
-                        : 'opacity-50 hover:ml-[8px] hover:opacity-100 focus:ml-[8px] focus:opacity-100'
-                    }`}
+            className={`relative flex items-center text-left
+                      text-[20px] font-extralight uppercase leading-[0.85] transition-all duration-300 
+                      md:text-[22px] md:leading-[0.82]
+                     ${index === 2 ? 'w-[133px]  md:w-[149px] ' : ''}
+                     ${
+                       currentSlide === index
+                         ? 'btn ml-[16px] font-medium'
+                         : 'opacity-50 hover:ml-[8px] hover:opacity-100 focus:ml-[8px] focus:opacity-100'
+                     }`}
             type="button"
             onClick={() => onSlideTo(index)}
           >
