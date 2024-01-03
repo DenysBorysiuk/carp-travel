b013e8e71ee80ea433ab7e6ce4ff0dcaa2782f88
Denys Borysiuk <denisborisiuk@gmail.com>
2024-01-03 10:36:30 +0200

    fix logo aria

--- src/components/ui/Logo/Logo.tsx
+++ src/components/ui/Logo/Logo.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 
 const Logo = () => {
   return (
-    <Link href="/" className="flex w-[61px] flex-col gap-[2px]" aria-label="link with company logo">
+    <Link href="/" className="flex w-[61px] flex-col gap-[2px]" aria-label="Company logo">
       <Image src="/icons/logo.png" width={59} height={21} alt="logo" priority />
       <span className="font-karantina font-normal leading-none tracking-[2.59px]">CarpTravel</span>
     </Link>
