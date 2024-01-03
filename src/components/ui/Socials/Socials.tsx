368fa0e7c9e69496edd4897fd85aa2d3f6009b14
Denys Borysiuk <denisborisiuk@gmail.com>
2024-01-03 13:40:41 +0200

    fix social links

--- src/components/ui/Socials/Socials.tsx
+++ src/components/ui/Socials/Socials.tsx
@@ -7,7 +7,7 @@ const Socials = () => {
   return (
     <div className="flex gap-[20px] xl:mr-[30px] xl:flex-row-reverse smOnly:mr-[15px] smOnly:justify-end">
       <p className="mt-[4px] text-[12px] xl:leading-loose">{text}</p>
-      <ul>
+      <ul className=" xl:text-right">
         {links.map(({ label, href }) => (
           <li key={label}>
             <a
